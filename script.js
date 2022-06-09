3fadb6d22ceb25e68042d20758a4e73013f4512a
Mustafa Karadeniz <65789027+karadnz@users.noreply.github.com>
2022-06-09 18:09:09 +0300

    updated date handling

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ let pompaFiyat=0;
 
 function getPrice (){
 
+    //parse ekle
+    var today = new Date();
+
+    var day= today.getDate()>10 ? today.getDate() : "0"+today.getDate();
+
+    var month= (today.getMonth()+1)>10 ? today.getMonth()+1 : "0"+(today.getMonth()+1);
+    
+    var date = day+"."+month+"."+today.getFullYear();
+    console.log(date);
+
+    var dateStart= `01.${month}.${today.getFullYear()}`;
+    console.log(dateStart)
+
+    const petrol="https://www.petrolofisi.com.tr/posvc/fiyat/gecmis?baslangic=01.06.2022&bitis=07.06.2022&il=ANKARA&ilce=CANKAYA";
+    const petrolDynamic=`https://www.petrolofisi.com.tr/posvc/fiyat/gecmis?baslangic=${dateStart}&bitis=${date}&il=ANKARA&ilce=CANKAYA`
+
 
     const onEk="https://api.allorigins.win/get?url=";
-    const petrol="https://www.petrolofisi.com.tr/posvc/fiyat/gecmis?baslangic=01.06.2022&bitis=07.06.2022&il=ANKARA&ilce=CANKAYA";
-    const sonEk=`${encodeURIComponent('https://www.petrolofisi.com.tr/posvc/fiyat/gecmis?baslangic=01.06.2022&bitis=07.06.2022&il=ANKARA&ilce=CANKAYA')}`;
+    
+    const sonEk=encodeURIComponent(`${petrolDynamic}`);
     const url=onEk+sonEk;
 
     
@@ -28,19 +44,25 @@ function getPrice (){
 
             console.log(json);
 
-            
+            //getting the last key from yakitJson object
+            var current= Object.keys(yakitJson)[Object.keys(yakitJson).length-1];
 
-            console.log(yakitJson["04.06.2022"][0]);
+            console.log(current);
+
+            console.log(yakitJson);
+            console.log(typeof(yakitJson));
+            console.log(yakitJson[current][0]);
 
             ic=`
             <ul class="list-group text-center">
 
-            <li > ${yakitJson["04.06.2022"][0].UrunAdi}</li>
-            <li > ${yakitJson["04.06.2022"][0].PompaFiyat}</li>
+            <li > Fiyat Tarihi: ${current}</li>
+            <li > ${yakitJson[current][0].UrunAdi}</li>
+            <li > ${yakitJson[current][0].PompaFiyat} ₺ </li>
             
             </ul>
             `
-            pompaFiyat= yakitJson["04.06.2022"][0].PompaFiyat;
+            pompaFiyat= yakitJson[current][0].PompaFiyat;
             
 
             document.getElementById("price").innerHTML=ic;
@@ -61,14 +83,14 @@ function getPrice (){
 
 function calculatePrice(){
 
-    if(isNaN(document.getElementById("gasInput").value))
+    if(isNaN(document.getElementById("litre2").value))
     {
         alert("numara gir");
     }
     else{
         
 
-        let price = document.getElementById("gasInput").value*pompaFiyat;
+        let price = document.getElementById("litre2").value*pompaFiyat;
 
         let price2=parseFloat(price.toPrecision(4))
 
@@ -80,5 +102,28 @@ function calculatePrice(){
 
 function updateLtText(val) {
     //document.getElementById('litre').innerHTML=`${val} lt`;
+
     document.getElementById('litre2').value=`${val}`; 
+
+    //0.5 olayini kaldir otomatik yuvarlasin daha akici olur
   }
+
+
+function updateLtRange(val) {
+    
+    
+document.getElementById('gasInput').value=`${val}`; 
+
+    
+}
+
+
+
+var slider = document.getElementById("gasInput");
+var output = document.getElementById("litre2");
+output.innerHTML = slider.value; 
+
+
+slider.oninput = function() {
+  output.value = this.value;
+}
